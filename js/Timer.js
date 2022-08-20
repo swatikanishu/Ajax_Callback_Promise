a200940e797c2dc1e95144d3b102b640766b219b
swatikanishu <nishuswatika@gmail.com>
2022-08-20 18:42:23 +0530

    Added:ajax pro gram is added

---
+++ js/Timer.js
@@ -0,0 +1,12 @@
+function showTime(){
+    const date=new Date();
+     return date.getHours()+"Hrs: " +date.getMinutes()+" Mins: " +date.getSeconds()+" Secs ";
+}
+
+ function showSessionExpire()  {
+     console.log("Activity-B : Your Session expired at "+showTime());
+}
+
+console.log("Activity -A : Triggering Activity-B At "+showTime());
+setTimeout(showSessionExpire,5000);
+ console.log("Activity -A : Triggering Activity-B At "+showTime()+" will execute after 5 sec");
